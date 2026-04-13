ea10e4d8a41f45b8816546762f66e16e419deeab
Cell6969 <bossmarinoo@gmail.com>
2026-04-13 13:28:32 +0700

    feat: add widget profile, card wallet and level

--- mobile/lib/ui/pages/home_page.dart
+++ mobile/lib/ui/pages/home_page.dart
@@ -61,6 +61,145 @@ class HomePage extends StatelessWidget {
         child: Image.asset('assets/ic_plus_circle.png', width: 24),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        children: [buildProfile(), buildWalletCard(), buildLevel()],
+      ),
     );
+  }
+
+  Widget buildProfile() {
+    return Container(
+      margin: const EdgeInsets.only(top: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Howdy', style: greyTextStyle.copyWith(fontSize: 16)),
+              const SizedBox(height: 2),
+              Text(
+                'Shania Deb',
+                style: blackTextStyle.copyWith(
+                  fontSize: 20,
+                  fontWeight: semiBold,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: 60,
+            height: 60,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage('assets/img_profile.png'),
+              ),
+            ),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 16,
+                height: 16,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: whiteColor,
+                ),
+                child: Center(
+                  child: Icon(Icons.check_circle, color: greenColor, size: 14),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildWalletCard() {
+    return Container(
+      height: 220,
+      width: double.infinity,
+      margin: const EdgeInsets.only(top: 30),
+      padding: const EdgeInsets.all(30),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(28),
+        image: const DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage('assets/img_bg_card.png'),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Shania Deborah',
+            style: whiteTextStyle.copyWith(fontSize: 18, fontWeight: medium),
+          ),
+          const SizedBox(height: 28),
+          Text(
+            '**** **** **** 1234',
+            style: whiteTextStyle.copyWith(
+              fontSize: 18,
+              fontWeight: medium,
+              letterSpacing: 6,
+            ),
+          ),
+          const SizedBox(height: 21),
+          Text('Balance', style: whiteTextStyle),
+          Text(
+            'Rp. 12.500',
+            style: whiteTextStyle.copyWith(fontSize: 24, fontWeight: semiBold),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildLevel() {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.all(24),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: whiteColor,
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                'Level 1',
+                style: blackTextStyle.copyWith(fontWeight: medium),
+              ),
+              const Spacer(),
+              Text(
+                '50% ',
+                style: greenTextStyle.copyWith(fontWeight: semiBold),
+              ),
+              Text(
+                'of 20.000',
+                style: blackTextStyle.copyWith(fontWeight: semiBold),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(55),
+            child: LinearProgressIndicator(
+              value: 0.55,
+              minHeight: 5,
+              valueColor: AlwaysStoppedAnimation(greenColor),
+              backgroundColor: lightBackgroundColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildService() {
+    return Container();
   }
 }
