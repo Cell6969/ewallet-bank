871b283d97fff8373cec1dd344337602415b1495
Cell6969 <bossmarinoo@gmail.com>
2026-04-16 11:59:28 +0700

    feat: finish pin page

--- mobile/lib/ui/pages/pin_page.dart
+++ mobile/lib/ui/pages/pin_page.dart
@@ -1,48 +1,119 @@
 import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/widgets/buttons.dart';
 import 'package:flutter/material.dart';
 
-class PinPage extends StatelessWidget {
+class PinPage extends StatefulWidget {
   const PinPage({super.key});
+
+  @override
+  State<PinPage> createState() => _PinPageState();
+}
+
+class _PinPageState extends State<PinPage> {
+  final TextEditingController pinController = TextEditingController(text: '');
+
+  addPin(String number) {
+    if (pinController.text.length < 6) {
+      setState(() {
+        pinController.text = pinController.text + number;
+      });
+    }
+  }
+
+  deletePin() {
+    if (pinController.text.isNotEmpty) {
+      setState(() {
+        pinController.text = pinController.text.substring(
+          0,
+          pinController.text.length - 1,
+        );
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    pinController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: darkBackgroundColor,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Input PIN',
-              style: whiteTextStyle.copyWith(
-                fontSize: 20,
-                fontWeight: semiBold,
-              ),
-            ),
-            const SizedBox(height: 72),
-            SizedBox(
-              width: 200,
-              child: TextFormField(
-                obscureText: true,
-                obscuringCharacter: '*',
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 58),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Input PIN',
                 style: whiteTextStyle.copyWith(
-                  fontSize: 36,
-                  fontWeight: medium,
-                  letterSpacing: 16,
+                  fontSize: 20,
+                  fontWeight: semiBold,
                 ),
-                cursorColor: greyColor,
-                decoration: InputDecoration(
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: greyColor),
+              ),
+              const SizedBox(height: 72),
+              SizedBox(
+                width: 200,
+                child: TextFormField(
+                  controller: pinController,
+                  obscureText: true,
+                  obscuringCharacter: '*',
+                  style: whiteTextStyle.copyWith(
+                    fontSize: 36,
+                    fontWeight: medium,
+                    letterSpacing: 16,
                   ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: greyColor),
+                  enabled: false,
+                  cursorColor: greyColor,
+                  decoration: InputDecoration(
+                    disabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: greyColor),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: greyColor),
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 66),
-          ],
+              const SizedBox(height: 66),
+              Wrap(
+                spacing: 40,
+                runSpacing: 40,
+                children: [
+                  CustomPinButton(title: '1', onTap: () => addPin('1')),
+                  CustomPinButton(title: '2', onTap: () => addPin('2')),
+                  CustomPinButton(title: '3', onTap: () => addPin('3')),
+                  CustomPinButton(title: '4', onTap: () => addPin('4')),
+                  CustomPinButton(title: '5', onTap: () => addPin('5')),
+                  CustomPinButton(title: '6', onTap: () => addPin('6')),
+                  CustomPinButton(title: '7', onTap: () => addPin('7')),
+                  CustomPinButton(title: '8', onTap: () => addPin('8')),
+                  CustomPinButton(title: '9', onTap: () => addPin('9')),
+                  const SizedBox(width: 60),
+                  CustomPinButton(title: '0', onTap: () => addPin('0')),
+                  GestureDetector(
+                    onTap: () {
+                      deletePin();
+                    },
+                    child: Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: numberBackgroundColor,
+                      ),
+                      child: Center(
+                        child: Icon(Icons.arrow_back, color: whiteColor),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
